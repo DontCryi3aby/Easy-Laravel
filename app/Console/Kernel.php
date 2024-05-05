03630fe2735f5f9260f6923c8b9e067e9a1663b1
thachtryhard <i3oyhp@gmail.com>
2024-05-05 17:06:38 +0700

    Handle update published field of a Post when publishedAt in the future

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\Post;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,17 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // Handle publish post with Scheduling
+        // If post's publishedAt in future, then 1 minute after update published to 1
+        $schedule->call(function () {
+            $posts = Post::where('publishedAt', '<', now()->toDate())->where('published', 0)->get();
+            if(count($posts) > 0) {
+                foreach ($posts as $post) {
+                    $post->update(['published' => 1]);
+                }
+            }
+        })->everyMinute();
     }
 
     /**
